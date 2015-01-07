fc17fdfeac40e78f6800d07c179b3d4dffec2152
Neil Kim Gardose <neil.sprdout@gmail.com>
2015-01-07 16:40:46 +0800

    Add module and test

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+
+function comparator(a, b) { return a - b; }
+module.exports = function (list, cmp) {
+  cmp = cmp || comparator;
+
+  var j, 
+      itemToInsert, 
+      stillLooking;
+
+  for(var i = 1, size = list.length; i < size; i++) {
+    itemToInsert = list[i];
+    j = i - 1;
+    stillLooking = true;
+
+    while((j >= 0) && stillLooking) {
+      if(cmp(itemToInsert, list[j]) < 0) {
+        list[j + 1] = list[j];
+        j--;
+      } else {
+        stillLooking = false;
+      }
+    }
+    list[j + 1] = itemToInsert;
+  }
+
+  return list;
 };
--- test/test.js
+++ test/test.js
@@ -3,9 +3,52 @@
 var assert = require('assert');
 var insertionsort = require('../');
 
+var people = [
+  {name: 'Sindre', age: 30, place: 2},
+  {name: 'Passy', age: 25, place: 1},
+  {name: 'Stephen', age: 40, place: 4},
+  {name: 'Matt', age: 35, place: 3}
+];
+
+function desc(a, b) {
+  return b - a;
+}
+
+function objComparator(a, b) {
+  return a.age - b.age;
+}
+
 describe('insertionsort node module', function () {
-  it('must have at least one test', function () {
-    insertionsort();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+  it('should work with empty arrays', function() {
+    assert.deepEqual(insertionsort([]), []);
+  });
+
+  it('should sort the given array', function() {
+    assert.deepEqual(insertionsort([3, 5, 2, 1, 2]), [1, 2, 2, 3, 5]);
+    assert.deepEqual(insertionsort([100, 90, 20, 233, 13]), [13, 20, 90, 100, 233]);
+    assert.deepEqual(insertionsort([100, 90, 20, 233, 13]), [13, 20, 90, 100, 233]);
+  });
+
+  it('should sort the given array in ascending order', function() {
+    assert.deepEqual(insertionsort([3, 1, 4, 1, 5, 9, 2, 6, 5, 4]), [1, 1, 2, 3, 4, 4, 5, 5, 6, 9]);
+    assert.deepEqual(insertionsort([9, 2, 8, 6, 1, 3]), [1, 2, 3, 6, 8, 9]);
+    assert.deepEqual(insertionsort([5, 2, 2, 6, 1, 3]), [1, 2, 2, 3, 5, 6]);
+    assert.deepEqual(insertionsort([5, 2, 4, 6, 1, 3]), [1, 2, 3, 4, 5, 6]);
+    assert.deepEqual(insertionsort([0, 0, 0, 0, 0, -1]), [-1, 0, 0, 0, 0, 0]);
+  });
+
+  it('should sort the given array in descending order', function () {
+    assert.deepEqual(insertionsort([4, 2, 2, 6, 1, 3], desc), [6, 4, 3, 2, 2, 1]);
+    assert.deepEqual(insertionsort([0, 0, 0, 0, 0, 1], desc), [1, 0, 0, 0, 0, 0]);
+    assert.deepEqual(insertionsort([0, 0, 0, 0, 0, -1], desc), [0, 0, 0, 0, 0, -1]);
+  });
+
+  it('should work with array of objects in ascending order', function() {
+    assert.deepEqual(insertionsort(people, objComparator), [
+      {"name": 'Passy', "age": 25, "place": 1},
+      {"name": 'Sindre', "age": 30, "place": 2},
+      {"name": 'Matt', "age": 35, "place": 3},
+      {"name": 'Stephen', "age": 40, "place": 4}]
+    );
   });
 });
